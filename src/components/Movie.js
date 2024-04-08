a95b370247ed80aa8badfdb1ed7566f98694abb6
ggang89 <ggang89@gmail.com>
2024-04-08 23:25:29 +0900

    movie 컴포넌트 css 연결

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,19 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
 function Movie({ id, year, coverImg, title, summary, genres, rating }) {
   return (
-    <div>
-      <img src={coverImg} alt={title} />
-      <h2>{title}</h2>
+    <div className="movie_box">
+      <img className="movie_img" src={coverImg} alt={title} />
+      <div className="movie_info">
+      <h2 className="movie_title">{title}</h2>
       <h3>{year}</h3>
-      <h3>{rating}</h3>
-      <p>{summary.length > 235 ? `${summary.slice(0, 235)}.....` : summary}</p>
-      <ul>
+      <h3 className="movie_rating">rating: {rating}/10</h3>
+      </div>
+      <ul className="genres">
         {genres.map((g) => (
-          <li key={g}>{g}</li>
+          <li className="genres_list" key={g}>{g}</li>
         ))}
       </ul>
+      <p>{summary.length > 235 ? `${summary.slice(0, 235)}.....` : summary}</p>
     </div>
   );
 }
